c696eed7ac78cd0a83baf3ccf4c2c7459e6942c9
Muhammed shahas <Mshahas7886@gmail.com>
2021-03-21 20:56:44 +1300

    database need to set

--- src/MangooseSchema.js
+++ src/MangooseSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// const passportLocalMongoose = require("passport-local-mongoose");
 
 const postSchema = new Schema({
     ProjectName:{
@@ -23,3 +24,18 @@ const postSchema = new Schema({
 });
 
 module.exports = mongoose.model('Post', postSchema);
+
+
+// const mongoose = require("mongoose");
+// const passportLocalMongoose = require("passport-local-mongoose");
+
+// const UserSchema = new mongoose.Schema({
+//     username:String,
+//     password:String,
+//     phone:Number
+// }) ;
+
+//add a hash and salt field to our Schema in order to store the hashed password and the salt value.
+// UserSchema.plugin(passportLocalMongoose); 
+
+// module.exports = mongoose.model("User",UserSchema);
